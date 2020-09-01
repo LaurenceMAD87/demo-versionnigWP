278f4d47a2f4f7c173c40b5507c23c6c4192ec95
Madlibriste <laurencemad87@gmail.com>
2020-09-01 10:15:07 +0200

    add plugin infos

---
+++ demo-functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* demo-versionningWP functions and definitions
+*
+* @link https://developer.wordpress.org/themes/basics/theme-functions/
+*
+* @package WordPress
+* @subpackage demo-versionningWP
+* @since demo-versionningWP 1.0
+*/
+
+/**
+* demo-versionningWP only works in WordPress 4.7 or later.
+*/
+?>
